835f664786d7b56e9f6ac0b340f2b037494fed60
Yish Shi <yish0000@foxmail.com>
2019-07-31 17:00:48 +0800

    transfer to another repository

---
+++ checkin_everyday.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set PROJECT_PATH=D:\Project\python\autocheckin
+
+echo schtasks /Create /f /tn checkin_everyday /sc daily /st 12:00 /tr "%PROJECT_PATH%\checkin.bat"
+schtasks /Create /f /tn checkin_everyday /sc daily /st 12:00 /tr "%PROJECT_PATH%\checkin.bat"
+
+echo schtasks /Create /f /tn checkout_everyday /sc daily /st 12:00 /tr "%PROJECT_PATH%\checkout.bat"
+schtasks /Create /f /tn checkout_everyday /sc daily /st 12:00 /tr "%PROJECT_PATH%\checkout.bat"
+
+pause
---
+++ checkout.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set USERNAME=shizhenhua
+set PASSWORD=yish8866983
+set PROJECT_PATH=D:\Project\python\autocheckin
+
+set /a num=%RANDOM%%%60
+if %num% lss 10 (set num=0%num%)
+schtasks /Create /f /tn checkout /sc once /st 21:%num% /tr "%PROJECT_PATH%\auto_checkin.exe %USERNAME% %PASSWORD%"
+
+pause
