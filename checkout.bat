dfe5b59cb0d77cf111bc3b64a9db4df32a4cbf60
Yish Shi <yish0000@foxmail.com>
2019-11-11 09:46:36 +0800

    周六日也不签退

--- checkout.bat
+++ checkout.bat
@@ -4,6 +4,9 @@ set USERNAME=shizhenhua
 set PASSWORD=yish8866983
 set PROJECT_PATH=D:\Project\python\autocheckin
 
+if %date:~-2% == ÷‹¡˘ (exit 0)
+if %date:~-2% == ÷‹»’ (exit 0)
+
 set /a num=%RANDOM%%%60
 if %num% lss 10 (set num=0%num%)
 schtasks /Create /f /tn checkout /sc once /st 21:%num% /tr "%PROJECT_PATH%\auto_checkin.exe %USERNAME% %PASSWORD%"
